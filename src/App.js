89ad43bfa5afcd703c4c91f5ed56f6e0cea63a66
minaje0917 <k050918@naver.com>
2022-08-08 11:48:42 +0900

    :sparkles: add css

--- src/App.js
+++ src/App.js
@@ -1,25 +1,18 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
+import React, { Fragment } from 'react';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  const name = 'React'
+  // const style = {
+  //   backgroundColor:'black',
+  //   color:'aqua',
+  //   fontSize:'48px',
+  //   fontWeight:'bold',
+  //   padding:16
+  // };
+
+  return <div className='react'>{name}</div>
 }
 
 export default App;
