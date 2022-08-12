d7f5e643c8f713a4e29754d4367e222efaed6720
minaje0917 <k050918@naver.com>
2022-08-12 20:01:09 +0900

    React

--- src/Average.js
+++ src/Average.js
@@ -1,7 +1,7 @@
 import { useState } from "react";
 
 const getAverage = numbers => {
-    console.log('평균값 계산 중')
+    console.log('평균값 계산 중...')
     if (numbers.length === 0) return 0;
     const sum = numbers.reduce((a,b)=> a + b)
     return sum / numbers.length;
@@ -15,7 +15,7 @@ const Average = () => {
         setNumber(e.target.value);
     };
 
-    const onInsert = e => {
+    const onInsert = () => {
         const nextList = list.concat(parseInt(number));
         setList(nextList);
         setNumber('');
